d31031c63bcdc9997eebfb78eff7c20e18da7c82
Fabian <bucaramanga246@gmail.com>
2024-10-15 07:18:12 -0500

    dia15

--- tiposDeDatos/listas.py
+++ tiposDeDatos/listas.py
@@ -1,9 +1,12 @@
-lista = [ 11,22,33,44,55,66,77,88,99,22,33,22]
-lista2 = ["alejandro", "alberto", "cristiano", "artemisa", "poseidon"]
-print(lista)
-# elemento = input("ingrese el valor a agregar en la lista: ")
-# lista.remove(float(elemento))
+# lista = [ 11,22,33,44,55,66,77,88,99,22,33,22]
+# lista2 = ["alejandro", "alberto", "cristiano", "artemisa", "poseidon"]
+# print(lista)
+# # elemento = input("ingrese el valor a agregar en la lista: ")
+# # lista.remove(float(elemento))
+# # print(lista)
+
+# lista.sort(reverse=False)
 # print(lista)
 
-lista.sort(reverse=False)
-print(lista)
+
+
