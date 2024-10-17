a3e41889a0475ba0ed9812b160a5d4911bb87f88
Fabian <bucaramanga246@gmail.com>
2024-10-17 12:36:00 -0500

    cambios

--- datos/PruebaPanda.py
+++ datos/PruebaPanda.py
@@ -1,9 +1,36 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # import os
 # print(os.getcwd())
 
-df = pd.read_csv("happy.csv")
+# import os
+# print(os.getcwd())
 
-# Mostrar el DataFrame
-print(df['Country'])
+
+# lista = [1,2,3,4,5,6,7,8,9]
+
+# print(lista)
+# #crear una serie
+# mi_serie = pd.Series(lista)
+df = pd.read_csv("archivoss/poblacion.csv")
+
+# # Mostrar el DataFrame
+# print(df.head())
+# print(df[["Date","COL","ARG"]].mean)
+
+colombia = df["COL"]
+#print(colombia.mean()) # para colcoar la media de toda la columna
+
+# filtra que la poblacion sea mayor a 30 millones
+# filtrar = df[colombia>30000000]
+# print(filtrar["ARG"])
+
+
+
+#eliminar columnas
+# ElmFra = df.drop(columns=['FRA'])
+# print(ElmFra)
+df.plot(x='Date', y='CHN', figsize=(10, 6))
+plt.show()
---
+++ datos/ejemplo.py
@@ -0,0 +1,23 @@
+
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+x = [1,2,3,4]
+y = [1,3,9,16]
+
+
+df = pd.read_csv("archivoss/poblacion.csv")
+
+colombia = df["COL"]
+arg = df["ARG"]
+fecha = df["Date"]
+
+plt.plot(fecha,colombia, label='Colombia', color='blue', linestyle='--',linewidth=2)
+plt.plot(fecha,arg, label='Argentina', color='green', linestyle='-', linewidth=2)
+plt.xlabel('Eje X')
+plt.ylabel('Eje Y')
+
+plt.title('TITULO')
+plt.show()
